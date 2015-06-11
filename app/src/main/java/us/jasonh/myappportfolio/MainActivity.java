35eb72dd482ddee321d9329757719349d7d8ac41
Jason Hartley <jasonhartley@gmail.com>
2015-06-11 00:57:41 -0400

    initial commit

---
+++ app/src/main/java/us/jasonh/myappportfolio/MainActivity.java
@@ -0,0 +1,42 @@
+package us.jasonh.myappportfolio;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void showToast(View view) {
+        Button button = (Button) view;
+        Toast.makeText(getApplicationContext(), "This button will launch " + button.getText(),
+                Toast.LENGTH_SHORT).show();
+    }}
